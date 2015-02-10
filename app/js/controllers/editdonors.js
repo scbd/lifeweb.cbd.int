f4fee7c3d5794aef7dfb20ca4df42932d452faf9
Jason McCarrell <jason.abz@gmail.com>
2015-02-10 16:30:56 -0500

    able to delete donors from the editdonors page

--- app/js/controllers/editdonors.js
+++ app/js/controllers/editdonors.js
@@ -4,6 +4,14 @@ define(['app', '/app/js/directives/editdonor.js', ], function(app) {
     IStorage.drafts.query('(type eq \'lwDonor\')', {cahce: false}).then(function(result) {
         $scope.donors = result.data.Items;
         console.log('donors: ', result);
+
+        $scope.deleteDonor = function(donor) {
+          IStorage.drafts.delete(donor.identifier)
+            .then(function(response, status) {
+              console.log('del donor: ', response);
+              $scope.donors.splice($scope.donors.indexOf(donor), 1);
+            });
+        };
     });
   });
 });
