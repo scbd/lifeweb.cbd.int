948def898b2e637cd6800d186619be4df8e09e9e
Jason McCarrell <jason.abz@gmail.com>
2015-02-26 20:43:37 -0500

    added campaigns to the admin page.

--- app/js/controllers/admin.js
+++ app/js/controllers/admin.js
@@ -26,37 +26,36 @@ define(['app', '/app/js/controllers/editdonors.js'], function(app, map) {
       });
       */
 
-
-
-    //$http.get('http://localhost:1818/organizations')
-    query = '(type eq \'lwOrganization\')';
+    query = '(type eq \'lwCampaign\')';
     IStorage.drafts.query(query, draftParams)
       .then(function(response) {
-        console.log('orgs: ', response.data.Items);
-        $scope.organizations = response.data.Items;
-      })
-      /*
-      .error(function(response, status, headers, config) {
-          console.log('*ERROR* Response (code '+status+'): ', response);
+        console.log('draft campaigns: ', response.data.Items);
+        $scope.draftCampaigns = response.data.Items;
       });
-      */
+    //IStorage.documents.query(query, draftParams)
+    $http.get('http://localhost:2020/api/v2013/documents/?$filter=(type+eq+%27lwCampaign%27)&body=true&cache=true&collection=my')
+        .then(function(response) {
+            console.log('published campaigns: ', response.data.Items);
+            $scope.publishedCampaigns = response.data.Items;
+        });
 
     //TODO: I can just put a delete draft here =P
-    $scope.deleteProject = function(project) {
+    $scope.deleteProject = function(project, projects) {
       //$http.delete('http://localhost:1818/projects') //how did this only delete one? >>;;
       IStorage.drafts.delete(project.identifier)
         .then(function(response, status) {
           console.log('del proj: ', response);
-          $scope.projects.splice($scope.projects.indexOf(project), 1);
+          $scope.projects.splice(projects.indexOf(project), 1);
         });
     };
 
-    $scope.deleteOrganiation = function(organization) {
-      //$http.delete('http://localhost:1818/organization')
-      IStorage.drafts.delete(organization.identifier)
-        .success(function(response, status) {
-          console.log('del org: ', response);
-          $scope.organizations.splice($scope.organizations.indexOf(organization), 1);
+    //TODO: I can just put a delete draft here =P
+    $scope.deleteCampaign = function(campaign, campaigns) {
+      //$http.delete('http://localhost:1818/projects') //how did this only delete one? >>;;
+      IStorage.drafts.delete(campaign.identifier)
+        .then(function(response, status) {
+          console.log('del camp: ', response);
+          $scope.projects.splice(campaigns.indexOf(project), 1);
         });
     };
   });
