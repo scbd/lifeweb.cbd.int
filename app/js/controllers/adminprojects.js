86e103cc29a114ff50b3fcb804ff9e9ec38b865b
Jason McCarrell <jason.abz@gmail.com>
2015-03-24 14:15:38 -0400

    explore page is much fastr

--- app/js/controllers/adminprojects.js
+++ app/js/controllers/adminprojects.js
@@ -1,6 +1,7 @@
 define(['app', '/app/js/services/filters/thumbnail.js', '/app/js/services/filters/page.js', '/app/js/services/filters/linkify.js',], function(app) {
     app.controller('AdminProjectsCtrl', function($scope, $http, $upload, $q, IStorage) {
         $scope.publishedProjectsPage = 0;
+        $scope.itemsPerPage = 10;
         $scope.sortTerm = '-updatedOn';
 
         $scope.firstPage = function() {
