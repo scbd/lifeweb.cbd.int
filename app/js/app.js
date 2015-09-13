7da127be2f3b7c8d7e6a41a8dce5bd86aeca946c
randyhoulahan <randy.houlahan@cbd.int>
2015-09-13 16:57:58 -0400

    update to LW realm.  Lowercase out of index

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,7 @@ define(['angular', 'ui-utils', 'angular-form-controls', 'ng-tags-input', 'angula
 
 	var app = Angular.module('app', ['ngRoute', 'ngSanitize', 'ng-breadcrumbs', 'ui.unique', 'formControls', 'ngTagsInput', 'angularFileUpload', 'ngLocalizer', 'ui.bootstrap', 'ngKookies']);
 
-    app.value('realm', 'CHM-DEV');
+    app.value('realm', 'chm-dev');
 
 	app.config(function($controllerProvider, $compileProvider, $provide, $filterProvider, $httpProvider, $kookiesProvider) {
 		// Allow dynamic registration
