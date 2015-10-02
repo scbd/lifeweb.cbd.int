71b0fcdb7bc72c4cfa6122d11557851c64b0bca3
unknown <randy.houlahan@Houlahan.biodiv.org>
2015-10-02 16:29:15 -0400

    Updated facet and made change to drop down as per request

--- app/js/services/leafletmapservice.js
+++ app/js/services/leafletmapservice.js
@@ -1,38 +1,41 @@
 define(['app', '/app/js/services/common.js'], function(app) { 'use strict';
 
   app.factory('leafletMapService', [ '$http',"realm", "commonjs", function($http, realm,  commonjs) {
-	
-	  		
+
+
 		  //============================================================
          // api_call - takes an api call to lwProjects
-         //============================================================         
-          var generateMap = function(q) {      
-             
-              var mapQuery = '/api/v2013/index/select?q=' + q + 
-              '&rows=500&sort=createdDate_dt+desc,+title_t+asc&start=0&wt=json&fl=budgetCost_ds,donatioFunding_ds,title_s,country_ss,createdDate_s,funding_status,identifier_s,'+
+         //============================================================
+          var generateMap = function(q) {
+
+              var mapQuery = '/api/v2013/index/select?q=' + q +
+              '&rows=500&sort=createdDate_dt+desc,+title_t+asc&start=0&wt=json&fl=identifier_s,budgetCost_ds,donatioFunding_ds,title_s,country_ss,createdDate_s,funding_status,identifier_s,'+
               'thumbnail_s,donor_ss,updatedDate_s,coordinates_s';
               return $http.get(mapQuery)
                           // .success(function(data) {
-                          //  var newData =  processMapItems(data.response.docs);      
-                          //  console.log(newData)                     
+                          //  var newData =  processMapItems(data.response.docs);
+                          //  console.log(newData)
                           //   return newData
-                          // });       
-          }// generateMap 
+                          // });
+          }// generateMap
 	     function processMap(items){
-         
-         return processMapItems(items);     
-         
+
+         return processMapItems(items);
+
        }
 			  //============================================================
-        // 
-        //============================================================         
-         function processMapItems(projects) {  
-                    var mapdata=[];
+        //
+        //============================================================
+         function processMapItems(projects) {
+//console.log(projects);
+                var mapdata=[];
+                if(projects)
                     projects.forEach(function(item) {
                         commonjs.getFundingStatus(item);
                         if(item.coordinates_s ){ // some had missing coordinates in index causing errors
                               if(angular.isString(item.coordinates_s))
                                 item.coordinates_s = JSON.parse(item.coordinates_s);
+
                               var geoJsonMapItem = {
                                 type: 'Feature',
                                 geometry:{coordinates:[item.coordinates_s.lng,item.coordinates_s.lat,], type:'Point'},
@@ -44,10 +47,10 @@ define(['app', '/app/js/services/common.js'], function(app) { 'use strict';
                                 thumbnail: item.thumbnail_s,
                                 title: item.title_s}
                               };
-            
+
                             if(item.funding_status) geoJsonMapItem['marker-color']='#000';
-                             mapdata.push(geoJsonMapItem); 
-                         }   
+                             mapdata.push(geoJsonMapItem);
+                         }
                     });//foreach
                     return mapdata;
           }// processMapItems
