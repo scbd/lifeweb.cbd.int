a072ce4456678b0c5e7cd60ec3165754f7fa40fa
unknown <randy.houlahan@Houlahan.biodiv.org>
2015-10-02 16:25:00 -0400

    Fix zoom on project page

--- app/js/controllers/eoidetail.js
+++ app/js/controllers/eoidetail.js
@@ -102,81 +102,14 @@ console.log('eoi',$scope.eoi);
 
                   var setview = function() {
                     if ($scope.geolocation)
-                      map.map.setView($scope.geolocation, 10);
-                  }
+                      map.map.setView($scope.geolocation, $scope.eoi.coordinates.zoom);
+                  };
                   if(map.map)
                     setview();
                   else
                     map.callback = setview;
               });
             });
-
-            //Is this necessary anymore??
-            //I think it was to allow people to look around the map to se other funding opportunities.
-            //TODO: get this working again, remove the first line return;
-            /*
-            $http.jsonp('https://www.cbd.int/cbd/lifeweb/new/services/web/mapdata.aspx?callback=JSON_CALLBACK')
-               .success(function (data) {
-                   $scope.mapdata = data;
-
-                   var map = new L.map('map');
-                   map.setView([25, 15], 3);
-
-                   if ($scope.eoi.lat && $scope.eoi.lng)
-                       map.setView([$scope.eoi.lat, $scope.eoi.lng], 8);
-
-                   //map.legendControl.addLegend(document.getElementById('legend-content').innerHTML);
-                   L.tileLayer('http://{s}.tiles.mapbox.com/v3/mcdias.map-ze88o7vz/{z}/{x}/{y}.png').addTo(map);
-
-                   var fundedIcon = L.icon({
-                       iconUrl: '/app/images/map/dark-marker-icon.png',
-                       shadowUrl: '/app/images/map/marker-shadow.png',
-                       iconAnchor: [0, 40], // point of the icon which will correspond to marker's location
-                       shadowAnchor: [0, 40],  // the same for the shadow
-                       popupAnchor: [13, -45] // point from which the popup should open relative to the iconAnchor
-
-                   });
-                   var notfundedIcon = L.icon({
-                       iconUrl: '/app/images/map/marker-icon.png',
-                       shadowUrl: '/app/images/map/marker-shadow.png',
-                       iconAnchor: [0, 40], // point of the icon which will correspond to marker's location
-                       shadowAnchor: [0, 40],  // the same for the shadow
-                       popupAnchor: [13, -45] // point from which the popup should open relative to the iconAnchor
-                   });
-
-                   function onEachFeature(feature, layer) {
-                       // Create custom popup content
-                       var popupContent = '<div id="' + feature.properties.title + '" class="popup" style="min-height:80px;">' +
-                                           '<a href="/project/?id=' + feature.properties.id + '">' +
-                                           '<img src="' + feature.properties.thumbnail + '" class="thumbnail pull-left" style="max-height:80px;max-width:80px;margin-right:10px;" alt="' + feature.properties.id + '"/>' +
-                                           '<strong>' + feature.properties.title + '</strong></a><br>' +
-                                           '<strong style="color:#660000;text-transform:uppercase;">' + feature.properties.funding_status + '</strong>' +
-                                           '<br>' +
-                                           '</div>';
-
-                       if (feature.properties && feature.properties.popupContent) {
-                           popupContent += feature.properties.popupContent;
-                       }
-
-                       layer.bindPopup(popupContent, {
-                           closeButton: false,
-                           minWidth: 320
-                       });
-                   }
-
-                   L.geoJson($scope.mapdata, {
-
-                       pointToLayer: function (feature, latlng) {
-                           if (feature.properties.funding_status == "funded")
-                               return L.marker(latlng, { icon: fundedIcon });
-                           else
-                               return L.marker(latlng, { icon: notfundedIcon });
-                       },
-                       onEachFeature: onEachFeature
-                   }).addTo(map);
-
-               });
-               */
     }
 
     function fillInDonorData() {
