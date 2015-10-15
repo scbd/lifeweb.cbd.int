bd863b17af1364329219cb9248762eb4fa20a733
unknown <randy.houlahan@Houlahan.biodiv.org>
2015-10-15 09:21:34 -0400

    fixed api call and country issue

--- app/js/controllers/donor.js
+++ app/js/controllers/donor.js
@@ -14,7 +14,7 @@ define(['app', 'authentication', '/app/js/services/filters.js', 'URI', 'editForm
     $http.get('/api/v2013/index/select?cb=1418322176016&q=((realm_ss:chm)%20AND%20(schema_s:lwProject)%20AND%20(donor_ss:'+sID+'))&rows=25&sort=createdDate_dt+desc,+title_t+asc&start=0&wt=json').then(function(data) {
        $http.get('/api/v2013/thesaurus/domains/countries/terms', { cache: true }).then(function(data2) {
           $scope.countries = data2.data;
-        $scope.matches = data.data.response.docs;
+          $scope.matches = data.data.response.docs;
             _.each($scope.matches,function(match){
                   match.countryLong=[];
                   _.each(match.country_ss, function (countryCode){
@@ -29,7 +29,7 @@ define(['app', 'authentication', '/app/js/services/filters.js', 'URI', 'editForm
       });
     });
 
-    $scope.count = function(o) { var c=0;for(var k in o)++c;return c;};
+        $scope.count = function(o) { var c=0;for(var k in o)++c;return c;};
 
 
 
