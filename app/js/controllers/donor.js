105b570d8c8105d037f85b775b49dbdd81d28996
Randy Houlahan <randy.houlahan@cbd.int>
2016-06-03 10:28:26 -0400

    On Donor pages (for example: https://lifeweb.cbd.int/donors?id=CFFBA402-03E7-9093-4069-AFB5CAAD564A): In the list of “Funded LifeWeb Projects” at present, all donations from all donors appear in this list. What we want is to list only the associated funding from that donor on this page. The table should have the following title: Funded LifeWeb Projects from [Name of Donor]

--- app/js/controllers/donor.js
+++ app/js/controllers/donor.js
@@ -1,37 +1,38 @@
-define(['app', 'authentication', '/app/js/services/filters.js', 'URI', 'editFormUtility', '/app/js/services/filters/linkify.js', '/app/js/directives/projecttable.js',], function(app) {
-  app.controller('DonorCtrl', function ($scope, $http, editFormUtility) {
-    var sID = new URI().query(true).id;
+define(['app', 'authentication', '/app/js/services/filters.js', 'URI', 'editFormUtility', '/app/js/services/filters/linkify.js', '/app/js/directives/projecttable.js', ], function(app) {
+    app.controller('DonorCtrl', function($scope, $http, editFormUtility) {
+        var sID = new URI().query(true).id;
 
 
-    $scope.DonorID = sID;
-    $scope.countries = [];
+        $scope.DonorID = sID;
+        $scope.countries = [];
 
-    editFormUtility.load(sID).then(function(data) {
-        console.log('donor data: ', data);
-        $scope.donor = data;
-    });
+        editFormUtility.load(sID).then(function(data) {
+            $scope.donor = data;
+        });
 
-    $http.get('/api/v2013/index/select?cb=1418322176016&q=((realm_ss:chm)%20AND%20(schema_s:lwProject)%20AND%20(donor_ss:'+sID+'))&rows=25&sort=createdDate_dt+desc,+title_t+asc&start=0&wt=json').then(function(data) {
-       $http.get('/api/v2013/thesaurus/domains/countries/terms', { cache: true }).then(function(data2) {
-          $scope.countries = data2.data;
-          $scope.matches = data.data.response.docs;
-            _.each($scope.matches,function(match){
-                  match.countryLong=[];
-                  _.each(match.country_ss, function (countryCode){
-                        _.each($scope.countries,function(country){
-                            if(country.identifier === countryCode)
-                              match.countryLong.push(country.name);
+        $http.get('/api/v2013/index/select?cb=1418322176016&q=((realm_ss:chm)%20AND%20(schema_s:lwProject)%20AND%20(donor_ss:' + sID + '))&rows=25&sort=createdDate_dt+desc,+title_t+asc&start=0&wt=json').then(function(data) {
+            $http.get('/api/v2013/thesaurus/domains/countries/terms', {
+                cache: true
+            }).then(function(data2) {
+                $scope.countries = data2.data;
+                $scope.matches = data.data.response.docs;
+                _.each($scope.matches, function(match) {
+                    match.countryLong = [];
+                    _.each(match.country_ss, function(countryCode) {
+                        _.each($scope.countries, function(country) {
+                            if (country.identifier === countryCode)
+                                match.countryLong.push(country.name);
                         });
-                  });
+                    });
+                });
             });
-          
- console.log('$scope.matches ', $scope.matches);
-      });
+        });
+
+        $scope.count = function(o) {
+            var c = 0;
+            for (var k in o) ++c;
+            return c;
+        };
+
     });
-
-        $scope.count = function(o) { var c=0;for(var k in o)++c;return c;};
-
-
-
-  });
 });
--- app/js/directives/projecttable.js
+++ app/js/directives/projecttable.js
@@ -1,54 +1,63 @@
-define(['app', '/app/js/services/filters.js', '/app/js/services/filters/thumbnail.js',], function(app) {
+define(['app', '/app/js/services/filters.js', '/app/js/services/filters/thumbnail.js', ], function(app) {
     app.directive('projecttable', function() {
         return {
             restrict: 'EAC',
             templateUrl: '/app/js/directives/projecttable.html',
             scope: {
                 projects: '=',
+                donorId: '=',
                 currency: '=?',
             },
             controller: function($scope) {
-              $scope.orderList = true;
-              $scope.sortTerm = 'createdDate_dt';
-              $scope.projects = $scope.projects || [];
 
-              $scope.currency = $scope.currency || 'USD';
+                $scope.orderList = true;
+                $scope.sortTerm = 'createdDate_dt';
+                $scope.projects = $scope.projects || [];
 
-              $scope.$watch('projects', function() {
-                if(!$scope.projects) return;
-                //TODO: use a filter for this instead i think...
-                for(var i=0; i!=$scope.projects.length; ++i) {
-                    if(!$scope.projects[i].totalCost) {
-                        $scope.projects[i].totalCost = 0;
-                        var funding = $scope.projects[i].budgetCost_ds || [];
-                        for(var k=0; k!=funding.length; ++k)
-                            $scope.projects[i].totalCost += funding[k];
+                $scope.currency = $scope.currency || 'USD';
+
+                $scope.$watch('projects', function() {
+                    if (!$scope.projects) return;
+                    //TODO: use a filter for this instead i think...
+                    for (var i = 0; i != $scope.projects.length; ++i) {
+                        if (!$scope.projects[i].totalCost) {
+                            $scope.projects[i].totalCost = 0;
+                            var funding = $scope.projects[i].budgetCost_ds || [];
+                            for (var k = 0; k != funding.length; ++k)
+                                $scope.projects[i].totalCost += funding[k];
+                        }
+                        $scope.projects[i].totalFunding = $scope.projects[i].totalFunding || 0;
+                        if (!$scope.projects[i].totalFunding)
+                            if ($scope.projects[i].donatioFunding_ds)
+                                for (var k = 0; k != $scope.projects[i].donatioFunding_ds.length; ++k)
+                                    $scope.projects[i].totalFunding += $scope.projects[i].donatioFunding_ds[k];
+
+                        $scope.projects[i].funding_needed = $scope.projects[i].totalCost - $scope.projects[i].totalFunding;
+                        //console.log('FUNDING NEEDED: ', $scope.projects[i].totalCost, $scope.projects[i].totalFunding, $scope.projects[i].funding_needed);
+
+                        if ($scope.projects[i].funding_needed < 1)
+                            $scope.projects[i].funding_status = 'funded';
+                        else if ($scope.projects[i].totalFunding < 1)
+                            $scope.projects[i].funding_status = 'not yet funded';
+                        else
+                            $scope.projects[i].funding_status = 'partially funded';
+
+                        _.each($scope.projects[i].donor_ss, function(val, key) {
+                            if (val === $scope.donorId)
+                                $scope.donation = $scope.projects[i].donatioFunding_ds[key];
+                        });
+
                     }
-                    $scope.projects[i].totalFunding = $scope.projects[i].totalFunding || 0;
-                    if(!$scope.projects[i].totalFunding)
-                        if($scope.projects[i].donatioFunding_ds)
-                            for(var k=0; k!=$scope.projects[i].donatioFunding_ds.length; ++k)
-                                $scope.projects[i].totalFunding += $scope.projects[i].donatioFunding_ds[k];
+                });
 
-                    $scope.projects[i].funding_needed = $scope.projects[i].totalCost - $scope.projects[i].totalFunding;
-//console.log('FUNDING NEEDED: ', $scope.projects[i].totalCost, $scope.projects[i].totalFunding, $scope.projects[i].funding_needed);
-
-                    if($scope.projects[i].funding_needed < 1)
-                        $scope.projects[i].funding_status = 'funded';
-                    else if($scope.projects[i].totalFunding < 1)
-                        $scope.projects[i].funding_status = 'not yet funded';
+                $scope.sortTable = function(term) {
+                    if ($scope.sortTerm == term) {
+                        $scope.orderList = !$scope.orderList;
+                    } else {
+                        $scope.sortTerm = term;
+                        $scope.orderList = true;
+                    }
                 }
-              });
-
-              $scope.sortTable = function (term) {
-                  if ($scope.sortTerm == term) {
-                      $scope.orderList = !$scope.orderList;
-                  }
-                  else {
-                      $scope.sortTerm = term;
-                      $scope.orderList = true;
-                  }
-              }
             },
         }
     });
